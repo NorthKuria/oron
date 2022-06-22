d36bf7205bd6a643c0cf33900dd7278359cc0078
northkuria <kuri4martin@gmail.com>
2022-06-22 12:23:19 +0300

    Added functionality to fetch data from covid 19 API

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 document.addEventListener('DOMContentLoaded', init)
 
 function init() {
-  console.log('here')
+  document.getElementById('myForm').onsubmit = function (e) {
+    myFunction(e)
+  }
+
+  function myFunction(e) {
+    e.preventDefault()
+    let countryValue = document.getElementById('country').value
+    const results = document.getElementById('results')
+
+    if (countryValue) {
+      fetch('https://api.covid19api.com/summary')
+        .then((res) => {
+          return res.json()
+        })
+        .then((data) => {
+          let resultsText = ''
+          data.Countries.forEach((item) => {
+            if (item.Country.toLowerCase() === countryValue.toLowerCase()) {
+              resultsText += '<h3>New Confirmed: ' + item.NewConfirmed + '</h3>'
+              resultsText += '<h3>New Deaths: ' + item.NewDeaths + '</h3>'
+              resultsText += '<h3>New Recovered' + item.NewRecovered + '</h3>'
+            }
+          })
+          results.innerHTML = resultsText
+        })
+    }
+  }
 }
